6c7e8b8119e41ed0d967441da9c9f6228279118a
Ethan Czahor <ethanczahor@gmail.com>
2023-10-08 09:28:24 -0700

    single image

--- app/wikidata-api.ts
+++ app/wikidata-api.ts
@@ -1,18 +1,25 @@
 import { inspect } from 'util'
 
 export const query = async (name: string) => {
-  const sparqlQuery = `SELECT DISTINCT ?item ?itemLabel ?itemDescription ?image WHERE {
-      SERVICE wikibase:mwapi {
-          bd:serviceParam wikibase:api "EntitySearch" .
-          bd:serviceParam wikibase:endpoint "www.wikidata.org" .
-          bd:serviceParam mwapi:search "${name}" .
-          bd:serviceParam mwapi:language "en" .
-          ?item wikibase:apiOutputItem mwapi:item .
-      }
-      ?item wdt:P31 wd:Q5 ;
-      OPTIONAL {?item wdt:P18 ?image}
-      SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
-    } LIMIT 20`
+  const sparqlQuery = `SELECT (SAMPLE(?item) as ?item) (SAMPLE(?itemLabel) as ?itemLabel) (SAMPLE(?itemDescription) as ?itemDescription) (MIN(?image) as ?mainImage) WHERE {
+  SERVICE wikibase:mwapi {
+    bd:serviceParam wikibase:api "EntitySearch" .
+    bd:serviceParam wikibase:endpoint "www.wikidata.org" .
+    bd:serviceParam mwapi:search "${name}" .
+    bd:serviceParam mwapi:language "en" .
+    ?item wikibase:apiOutputItem mwapi:item 
+  }
+  ?item wdt:P31 wd:Q5 .
+  ?item wdt:P18 ?image
+          
+  SERVICE wikibase:label { 
+    bd:serviceParam wikibase:language "en" .
+    ?item rdfs:label ?itemLabel .
+    ?item schema:description ?itemDescription
+  }
+} 
+GROUP BY ?item
+LIMIT 20`
 
   const fullUrl = 'https://query.wikidata.org/sparql' + '?query=' + encodeURIComponent(sparqlQuery)
   const headers = { Accept: 'application/sparql-results+json' }
@@ -23,7 +30,7 @@ export const query = async (name: string) => {
         item: { value: string }
         itemLabel: { value: string }
         itemDescription?: { value: string }
-        image?: { value: string }
+        mainImage?: { value: string }
       }[]
     }
   }
@@ -32,7 +39,7 @@ export const query = async (name: string) => {
     id: Number(b.item.value.match(/\/Q(.+?)$/)?.[1]),
     name: b.itemLabel?.value,
     description: b.itemDescription?.value,
-    imageUrl: b.image?.value,
+    imageUrl: b.mainImage?.value,
   }))
 }
 
